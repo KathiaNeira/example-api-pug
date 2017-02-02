d2c4867cea0ba523c05897327075c68f7013d08e
kathia <kathia.neira@orbis.com.pe>
2017-02-02 17:25:26 -0500

    feat(pug):api pug

---
+++ pugDemo.js
@@ -0,0 +1,28 @@
+/*
+ *@module (pug)
+ *@author Kathia Neira
+ */
+
+var pug = require('pug');
+var fs = require('fs');
+var glob = require("glob");
+var pattern = __dirname + "/frontend/pug/*.pug";
+var src = __dirname + "/src/html/";
+
+var filesInput = glob.sync(pattern);
+
+// Compile the source code
+filesInput.forEach(function(element, index) {
+    var path = element.split("/").pop();
+    var name = path.replace('pug', 'html');
+
+    var compiledFunction = pug.compileFile(element, {
+      filename : 'pug',
+      doctype: 'html',
+      pretty: true,
+      inlineRuntimeFunctions: true
+    });
+
+    var html = compiledFunction();
+    fs.writeFileSync(src.concat(name), html, 'utf-8');
+});
